6c89131e4610588039909ae70c17215c70934f6c
yipple <1275432232@qq.com>
2020-06-18 18:45:41 +0800

    修改主页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,9 +10,29 @@
 
         <div class="main-wrap">
             <el-container class="main-container">
-                <p>
-                    欢迎来到作业管理系统！
-                </p>
+                <div class="introduction-container">
+                    <p>
+                        欢迎来到作业管理系统！
+                    </p>
+                    <p>
+                        前端项目地址
+                        <el-link href="https://github.com/Yi1275432232/homework-management-system-front-end"
+                                 type="primary">
+                            homework-management-system-front-end
+                        </el-link>
+                    </p>
+                    <p>
+                        后端项目地址
+                        <el-link href="https://github.com/Yi1275432232/homework-management-system-back-end"
+                                 type="success">
+                            homework-management-system-back-end
+                        </el-link>
+                    </p>
+                    <p>
+                        我的博客地址
+                        <el-link href="https://blog.csdn.net/Yiang0" type="info">Yiang的博客</el-link>
+                    </p>
+                </div>
             </el-container>
         </div>
     </div>
@@ -42,7 +62,11 @@
     .main-container {
         width: 90%;
         height: 100%;
-        text-align: center;
+        text-align: justify-all;
         margin: auto;
+    }
+
+    .introduction-container {
+        font-size: 16px;
     }
 </style>
